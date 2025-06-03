2038ae5a47e0fbf2416121e3c80a1dfebc1daa6b
Diego Anunciacao <diegoeliasanunciacao@gmail.com>
2025-06-03 23:10:53 +0100

    updates email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,13 @@
 class ContactMailer < ApplicationMailer
   default to: "carterfotografia@gmail.com"
+   default from: "carterfotografia@gmail.com"
   def create_email(contact_params)
     @name = contact_params[:name]
-    @email = contact_params[:email]
+    @reply_email = contact_params[:email]
     @date = contact_params[:date]
     @location = contact_params[:location]
     @message = contact_params[:message]
 
-    mail(from: @email, subject: "New Site Contact")
+    mail(subject: "New Site Contact", reply_to: @reply_email)
   end
 end
