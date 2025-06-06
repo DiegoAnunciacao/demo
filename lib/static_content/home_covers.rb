74fce81e9b21f897b915dc903e205e9b8c2204eb
Diego Anunciacao <diegoeliasanunciacao@gmail.com>
2025-06-06 22:11:54 +0100

    updates gallery

--- lib/static_content/home_covers.rb
+++ lib/static_content/home_covers.rb
@@ -2,9 +2,9 @@ module StaticContent
   class HomeCovers
     def self.all
       [
-      { image: "HELEN.jpg" },
-      { image: "Wedding.jpg" },
-      { image: "WEDDING1.jpg" }
+      { image: "https://res.cloudinary.com/dpbweqf7f/image/upload/v1749244208/HELEN_uh0jq0.jpg" },
+      { image: "https://res.cloudinary.com/dpbweqf7f/image/upload/v1749244208/Wedding_d36pla.jpg" },
+      { image: "https://res.cloudinary.com/dpbweqf7f/image/upload/v1749244209/WEDDING1_zkw52h.jpg" }
     ]
     end
   end
